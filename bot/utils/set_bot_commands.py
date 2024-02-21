fcf60a75f69da149d0614d186b4ba438b07aaf74
nekoduykod <zaliubovskyio@ukr.net>
2024-02-21 22:53:55 +0200

    shortened descr

--- bot/utils/set_bot_commands.py
+++ bot/utils/set_bot_commands.py
@@ -6,10 +6,10 @@ from aiogram.types import BotCommand
 
 async def set_default_commands(bot: Bot) -> None:
     commands = [
-        BotCommand(command="start", description="Start the bot and access the main menu."),
-        BotCommand(command="help", description="Get instructions on how to use the bot."),
-        BotCommand(command="gpt_request", description="Request text generation from ChatGPT directly."),
-        BotCommand(command="exit", description="Return to the main menu.")
+        BotCommand(command="start", description="/start."),
+        BotCommand(command="help", description="instructions"),
+        BotCommand(command="gpt_request", description="request ChatGPT directly."),
+        BotCommand(command="exit", description="exits a state")
     ]
     try:
         await bot.set_my_commands(commands)
